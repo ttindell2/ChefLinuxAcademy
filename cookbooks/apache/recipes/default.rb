cc5af5072d284a74f96339f9ae172dbdc4351740
Tim Tindell <tim.tindell@gmail.com>
2017-05-04 22:00:58 +0000

    Added

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,8 +4,14 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-package 'apache2' do 
-	package_name 'httpd'
+if node['platform_family'] == "rhel"
+	package="httpd"
+elsif node['platform_family'] == "debian"
+	package="apache2"
+end
+
+package 'apache2' do
+	package_name package
 	action :install
 end
 
